e84aa3a6a281b52205bd5bfa7ff92c660b36da19
邓冬林 <972325986@qq.com>
2020-03-07 13:53:58 +0800

    第一次提交

---
+++ src/main/java/com/ddl/jvm/Main.java
@@ -0,0 +1,12 @@
+package com.ddl.jvm;
+
+/**
+ * @description 主类
+ * @author: dengdl
+ * @create: 2020-03-07 13:19
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
---
+++ src/main/java/com/ddl/jvm/MainFrame.java
@@ -0,0 +1,12 @@
+package com.ddl.jvm;
+
+import javax.swing.*;
+
+/**
+ * @description 主面板
+ * @author: dengdl
+ * @create: 2020-03-07 13:24
+ **/
+public class MainFrame extends JFrame {
+
+}
